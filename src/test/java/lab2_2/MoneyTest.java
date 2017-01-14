3f86c6f723c072ff62db6d6d9d3b3cf5bcaf56c6
Lukasz Szczepanek <lukas.szczepanek@gmail.com>
2017-01-14 16:20:44 +0100

    testing multiplay of value

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -12,12 +12,19 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 public class MoneyTest {
 	
 	
-	
 	@Test
 	public void testMoneyHaveProperFormat(){
 		Money mon = new Money(10, "USD");
 		
 		assertThat(mon.toString(), is(equalTo("10,00 USD")));
+	}
+	
+	@Test
+	public void testIsMultiplayWorkingGood(){
+		Money moneyA = new Money (20, "EUR");
+		Money result = moneyA.multiplyBy(5.0);
+		
+		assertThat(result.toString(), is(equalTo("100,00 EUR")));
 	}
 
 }
